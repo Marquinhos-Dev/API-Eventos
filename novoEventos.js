afbd61250fbb29024eb8ff54f2eacb6e26a0cded
Marquinhos-Dev <marcosconrado007@gmail.com>
2024-09-18 16:30:05 -0300

    oi

--- novoEventos.js
+++ novoEventos.js
@@ -33,6 +33,64 @@ const esquemaIngresso = new mongoose.Schema({
 const Evento = mongoose.model("Evento", esquemaEvento);
 const Ingresso = mongoose.model("Ingresso", esquemaIngresso);
 
+app.put("/ingressos/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { ID_evento, nomeUtilizador, idade, tipo } = req.body;
+      const ingressoAtualizado = await atualizarIngresso(id,ID_evento,nomeUtilizador,idade,tipo);
+      if (ingressoAtualizado) {
+        res.status(200).json({mensagem: "Ingresso atualizado com sucesso",atualizado: ingressoAtualizado,});
+      } else {
+        res.status(404).json({ mensagem: "Ingresso não encontrado" });
+      }
+    } catch (erro) {
+      res.status(500).json({ mensagem: "Erro ao atualizar ingresso", erro: erro.message });
+    }
+});
+
+app.put("/eventos/:id", async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { nomeEvento, artista, data, horario } = req.body;
+      const eventoAtualizado = await atualizarEvento(id,nomeEvento, artista, data, horario);
+      if (eventoAtualizado) {
+        res.status(200).json({mensagem: "Evento atualizado com sucesso",atualizado: eventoAtualizado,});
+      } else {
+        res.status(404).json({ mensagem: "Evento não encontrado" });
+      }
+    } catch (erro) {
+      res.status(500).json({ mensagem: "Erro ao atualizar evento", erro: erro.message });
+    }
+});
+
+async function atualizarIngresso(id,ID_evento,nomeUtilizador,idade,tipo) {
+    try {
+      const ingressoAtualizado = await Ingresso.findByIdAndUpdate(
+        id,
+        { ID_evento, nomeUtilizador, idade, tipo },
+        { new: true, runValidators: true }
+      );
+      return ingressoAtualizado;
+    } catch (erro) {
+      console.error("Erro ao atualizar ingresso:", erro);
+      throw erro;
+    }
+}
+
+async function atualizarEvento(id,nomeEvento, artista, data, horario) {
+    try {
+      const eventoAtualizado = await Evento.findByIdAndUpdate(
+        id,
+        { nomeEvento, artista, data, horario },
+        { new: true, runValidators: true }
+      );
+      return eventoAtualizado;
+    } catch (erro) {
+      console.error("Erro ao atualizar evento:", erro);
+      throw erro;
+    }
+}
+
 // Inicialização do servidor
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
