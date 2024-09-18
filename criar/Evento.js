c7f4a6d56cf282e0b3a39543ed002e671fa0d9ad
Laurabodewig0 <ar4546132@gmail.com>
2024-09-18 17:25:28 -0300

    criar

---
+++ criar/Evento.js
@@ -0,0 +1,8 @@
+let evento = [
+{id:1, NomeEvento:'falcao negro em perigo', Artista:' Ridley Scott', Data:'2001', Horario:'18:00'},
+{id:2, NomeEvento:'deixando o afeganistao', Artista:' Pavel Lungin', Data:'2019', Horario:'18:30'},
+{id:3, NomeEvento:'Resgate 2', Artista:' Sam Hargrave', Data:'2023', Horario:'20:00'}
+
+];
+
+module.exports = evento
---
+++ criar/Ingresso.js
@@ -0,0 +1,7 @@
+let ingresso = [
+    {id:1, nomeUtilizador:'Alexandre', Idade:'19', Tipo:'Adulto'},
+    {id:2, nomeUtilizador:'Felipe', Idade:'15', Tipo:'Adolescente'},
+    {id:3, nomeUtilizador:'Lucio', Idade:'10', Tipo:'criança'}
+];
+
+module.exports = ingresso
