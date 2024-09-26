50a47d7e4479429a6ec26a1e6d88b5ce6e8feb9b
Marcos Felipe Conrado <marcosconrado007@gmail.com>
2024-09-26 16:16:22 -0300

    últimos ajustes

--- v1.2 - modularização e MongoDB/server.js
+++ v1.2 - modularização e MongoDB/server.js
@@ -11,4 +11,4 @@ mongoose
 
 // Inicializando o servidor
 const port = 3000;
-app.listen(port, () => {console.log(`Example app listening on port ${port}`);});
+app.listen(port, () => {console.log(`Servidor rodando em: http://localhost:${port}/`)});
